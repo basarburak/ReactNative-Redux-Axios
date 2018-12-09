1244a9f4b2838acbb524b1f4c5a146c8b251a535
Burak Başar <basarburak@outlook.com>
2018-12-10 01:06:11 +0300

    add dependencies

---
+++ src/screens/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text> Home Screen </Text>
+            </View>
+        );
+    }
+}
